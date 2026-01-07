5183fd73611eead2de256d2f01e81b916c61f1ae
paytekat-code <paytekat@gmail.com>
2026-01-07 07:46:51 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,6 @@
 // js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 // Firebase configuration
